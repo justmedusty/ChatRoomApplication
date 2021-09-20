13f1a0f25bdd7fd4b2e88ea1db9028c9efba4d1c
Aguang Zhang <zhan0731@algonquinlive.com>
2021-09-20 17:19:57 -0400

    chat room template

--- src/main/java/org/acme/ReactiveGreetingResource.java
+++ src/main/java/org/acme/ReactiveGreetingResource.java
@@ -1,9 +1,16 @@
 package org.acme;
 
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("/hello")
 public class ReactiveGreetingResource {
